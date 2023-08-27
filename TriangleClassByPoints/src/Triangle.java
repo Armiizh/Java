455c17d3f5037a23c4bca12ba713fb4839b35ae2
Артемий <137889894+Armiizh@users.noreply.github.com>
2023-08-27 12:25:38 +0300

    Add files via upload

---
+++ TriangleClassByPoints/src/Triangle.java
@@ -0,0 +1,31 @@
+public class Triangle {
+    private final int sideA;
+    private final int sideB;
+    private final int sideC;
+
+    public Triangle (Point a, Point b, Point c) {
+        this(a.getDistanceTo(b),a.getDistanceTo(c),b.getDistanceTo(c));
+    }
+    public Triangle (int sideA, int sideB, int sideC) {
+        if (sideA > 0 && sideB > 0 && sideC > 0 &&
+                sideA + sideB > sideC &&
+                sideA + sideC > sideB &&
+                sideB + sideC > sideA) {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        } else {
+            throw new IllegalArgumentException("Invalid arguments "+sideA+ ", "+sideB+", "+sideC);
+        }
+    }
+    public String getDescription() {
+        return isRightTriangle()
+                ? "Прямоугольный треугольник"
+                : "Обычный треугольник со сторонами "+sideA+ ", "+sideB+ ", "+sideC;
+    }
+    private  boolean isRightTriangle() {
+        return this.sideA*this.sideA+this.sideB*this.sideB==this.sideC*this.sideC ||
+                this.sideA*this.sideA+this.sideC*this.sideC==this.sideB*this.sideB ||
+                this.sideB*this.sideB+this.sideC*this.sideC==this.sideA*this.sideA;
+    }
+}
